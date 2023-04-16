d90b45db2538f4daf96ad7969fe6a7d77eceb780
VeronicaRomio <veronicaromio.utn@gmail.com>
2023-04-15 22:26:54 -0300

    pseudoCaratula

---
+++ TP3 Sim/Form2.Designer.vb
@@ -0,0 +1,118 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form2
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form2))
+        Me.label3 = New System.Windows.Forms.Label()
+        Me.label2 = New System.Windows.Forms.Label()
+        Me.label1 = New System.Windows.Forms.Label()
+        Me.tituloSIM = New System.Windows.Forms.Label()
+        Me.btnInicio = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'label3
+        '
+        Me.label3.AutoSize = True
+        Me.label3.BackColor = System.Drawing.Color.Transparent
+        Me.label3.Font = New System.Drawing.Font("Tw Cen MT", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.label3.ForeColor = System.Drawing.Color.Black
+        Me.label3.Location = New System.Drawing.Point(136, 261)
+        Me.label3.Name = "label3"
+        Me.label3.Size = New System.Drawing.Size(552, 144)
+        Me.label3.TabIndex = 9
+        Me.label3.Text = resources.GetString("label3.Text")
+        '
+        'label2
+        '
+        Me.label2.AutoSize = True
+        Me.label2.BackColor = System.Drawing.Color.Transparent
+        Me.label2.Font = New System.Drawing.Font("Tw Cen MT", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.label2.ForeColor = System.Drawing.Color.Black
+        Me.label2.Location = New System.Drawing.Point(205, 206)
+        Me.label2.Name = "label2"
+        Me.label2.Size = New System.Drawing.Size(339, 24)
+        Me.label2.TabIndex = 8
+        Me.label2.Text = "                  INTEGRANTES:                "
+        '
+        'label1
+        '
+        Me.label1.AutoSize = True
+        Me.label1.BackColor = System.Drawing.Color.Transparent
+        Me.label1.Font = New System.Drawing.Font("Tw Cen MT", 18.75!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Underline), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.label1.ForeColor = System.Drawing.Color.Black
+        Me.label1.Location = New System.Drawing.Point(243, 79)
+        Me.label1.Name = "label1"
+        Me.label1.Size = New System.Drawing.Size(282, 29)
+        Me.label1.TabIndex = 7
+        Me.label1.Text = "Trabajo Práctico nº 2- 4k3"
+        '
+        'tituloSIM
+        '
+        Me.tituloSIM.AutoSize = True
+        Me.tituloSIM.BackColor = System.Drawing.Color.Transparent
+        Me.tituloSIM.Font = New System.Drawing.Font("Tw Cen MT", 18.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.tituloSIM.ForeColor = System.Drawing.Color.Black
+        Me.tituloSIM.Location = New System.Drawing.Point(226, 35)
+        Me.tituloSIM.Name = "tituloSIM"
+        Me.tituloSIM.Size = New System.Drawing.Size(331, 29)
+        Me.tituloSIM.TabIndex = 6
+        Me.tituloSIM.Text = "       SIMULACION- 2023         "
+        '
+        'btnInicio
+        '
+        Me.btnInicio.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink
+        Me.btnInicio.BackColor = System.Drawing.Color.RoyalBlue
+        Me.btnInicio.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.btnInicio.FlatAppearance.BorderSize = 3
+        Me.btnInicio.Font = New System.Drawing.Font("Tw Cen MT", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnInicio.ForeColor = System.Drawing.Color.White
+        Me.btnInicio.Location = New System.Drawing.Point(347, 444)
+        Me.btnInicio.Name = "btnInicio"
+        Me.btnInicio.Size = New System.Drawing.Size(97, 48)
+        Me.btnInicio.TabIndex = 5
+        Me.btnInicio.Text = "INICIAR"
+        Me.btnInicio.UseVisualStyleBackColor = False
+        '
+        'Form2
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 533)
+        Me.Controls.Add(Me.label3)
+        Me.Controls.Add(Me.label2)
+        Me.Controls.Add(Me.label1)
+        Me.Controls.Add(Me.tituloSIM)
+        Me.Controls.Add(Me.btnInicio)
+        Me.Name = "Form2"
+        Me.Text = "Form2"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Private WithEvents label3 As Label
+    Private WithEvents label2 As Label
+    Private WithEvents label1 As Label
+    Private WithEvents tituloSIM As Label
+    Public WithEvents btnInicio As Button
+End Class
---
+++ TP3 Sim/Form2.vb
@@ -0,0 +1,5 @@
+﻿Public Class Form2
+    Private Sub btnInicio_Click(sender As Object, e As EventArgs) Handles btnInicio.Click
+
+    End Sub
+End Class
